07dfe22c13ba5004fa703eebfd1173b656ead8e1
gerad216 <gerad216@gmail.com>
2018-07-05 23:27:32 +0900

    view 설정; JSP(주석처리); thymeleaf 적

--- SpringBlog/src/main/java/com/jskim216/blog/controller/HelloRestController.java
+++ SpringBlog/src/main/java/com/jskim216/blog/controller/HelloRestController.java
@@ -3,8 +3,10 @@ package com.jskim216.blog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jskim216.blog.domain.model.entity.Hello;
@@ -22,8 +24,8 @@ public class HelloRestController {
 		return helloData;
 	}
 
-	@RequestMapping("/list")
-	public List<Hello> list(Model model) {
+	@RequestMapping(value = "/list", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public @ResponseBody List<Hello> list(Model model) {
 		List<Hello> helloList = helloDao.findAll();
 		return helloList;
 	}
